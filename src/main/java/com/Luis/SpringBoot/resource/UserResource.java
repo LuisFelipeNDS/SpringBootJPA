1223be1a19924cfd036380000a338442431ec29a
Felipe <Felipesss.2014@gmail.com>
2023-07-10 21:37:43 -0300

    Service layer, component registration

--- src/main/java/com/Luis/SpringBoot/resource/UserResource.java
+++ src/main/java/com/Luis/SpringBoot/resource/UserResource.java
@@ -1,24 +1,51 @@
 package com.Luis.SpringBoot.resource;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.Luis.SpringBoot.entities.User;
+import com.Luis.SpringBoot.services.UserService;
 
 @RestController
 @RequestMapping(value="/users")
 public class UserResource {
 	
+	@Autowired
+	private UserService service;
 	
 	@GetMapping
-	public ResponseEntity<User> findAll(){ 
-		User u = new User(1L, "Luis", "Luis@gmail.com", "9999", "123");
-		//retorna obj do tipo ResponseEntity
+	public ResponseEntity<List<User>> findAll(){ 
+		List<User> list = service.findAll();	 	
 		// .ok() retorna a mensagem com sucesso para o http
 		// .body() retorna a variavel u
-		return ResponseEntity.ok().body(u);
+		return ResponseEntity.ok().body(list);
 	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<User> findById(@PathVariable Long id){
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
+	
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 
 }
---
+++ src/main/java/com/Luis/SpringBoot/services/UserService.java
@@ -0,0 +1,31 @@
+package com.Luis.SpringBoot.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.Luis.SpringBoot.entities.User;
+import com.Luis.SpringBoot.repositories.UserRepository;
+
+@Service
+//@Component registra a classe como um componente do Spring
+//@Service registra um servico na camada de servico
+public class UserService {
+	
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll(){
+		return repository.findAll();
+	}
+	
+	public User findById(Long id){
+		Optional<User> obj = repository.findById(id);
+		
+		return obj.get(); //Operacao get() do Optional retorna um objeto do tipo que foi atribuido
+	}				
+	
+	
+}
